7f5871ebbc2782ffb34aeedc794943280c36a3ae
michalsz24 <109131605+michalsz24@users.noreply.github.com>
2022-10-26 08:34:24 +0200

    Add files via upload
    init test

---
+++ E-ShopPlatform.API.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("E-ShopPlatform.API.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("E-ShopPlatform.API.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("371b5702-5a45-4f18-bd9f-c035be29fd6f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ E-ShopPlatform.API.Tests/TestPaymentController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using E_ShopPlatform.API.Controllers;
+using E_ShopPlatform.API.Models;
+using E_ShopPlatform.API.Enums;
+using E_ShopPlatform.API.Factories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using Moq;
+using E_ShopPlatform.API.Services;
+using E_ShopPlatform.API.Factories.PaymentGateway;
+
+namespace E_ShopPlatform.API.Tests
+{
+    [TestClass]
+    public class TestPaymentController
+    {
+        private readonly Order order = new Order() { Id = 1, UserId = 2, Amount = 50, Description = "Test Payment", PaymentGateway = PaymentTypes.FirstPaymentType};
+
+        [TestMethod]
+        public void CreatePayment_ShouldReturnReceiptAsBase64()
+        {
+            var mockPaymentFactory = new PaymentGatewayFactory();
+            var mockPaymentService = new PaymentService(mockPaymentFactory);
+            var mockPaymentController = new PaymentController(mockPaymentService);
+
+            mockPaymentController.Request = new HttpRequestMessage();
+            mockPaymentController.Configuration = new HttpConfiguration();
+
+            var response = mockPaymentController.CreatePayment(order);
+
+            Assert.IsTrue(response.TryGetContentValue<string>(out string receipt));
+            Assert.IsTrue(IsBase64String(receipt));
+        }
+
+        private bool IsBase64String(string base64)
+        {
+            try
+            {
+                Convert.FromBase64String(base64);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
